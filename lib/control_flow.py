eb6b21423a9543dcc841f19c4e1d8258490e5c7d
Parker <p.cronin.connolly@gmail.com>
2024-02-21 07:45:00 -0500

    functions lab wiht python 16 passing

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,19 +1,44 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+    return "Access denied" if (username != "admin" and username != "ADMIN") or password != "12345" else "Access granted"
+    
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif temperature > 40 and temperature < 65:
+        return "It's a little chilly out there!" 
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
+
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
+    
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    operations = {
+        "+" : num1 + num2,
+        "-" : num1 - num2,
+        "*" : num1 * num2,
+        "/" : num1 / num2
+    }
 
-print(calculator(/, 12, 6))
+    result = operations.get(operation)
+
+    if result == None:
+        print("Invalid operation!")
+        return None
+    
+    return result
+    
